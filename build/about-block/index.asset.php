add11118f22ce3fec616815bb37e6d846c97791d
Jasper Frumau <jasper@imagewize.com>
2025-01-29 12:46:43 +0700

    first commit

---
+++ build/about-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a0fd160012c2217a7491');
---
+++ build/about-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f191c2b5b8322664b1ad');
