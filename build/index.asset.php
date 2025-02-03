957e93fc932e8a7ebfe79821b6f51313d0d147a9
Jasper Frumau <jasper@imagewize.com>
2025-02-03 15:53:10 +0700

    core/group return for better alignment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => '043fb2ff2f58e03e3491');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => 'c1dfabd436cd9affe301');
