a89dfdde8c8afd82e19b9316db350d04f84ef3be
Jasper Frumau <jasper@imagewize.com>
2025-02-03 12:38:56 +0700

    proper namespace

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => '6a65dadae2ec9e356072');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => '6912d6a9461e323a9a29');
