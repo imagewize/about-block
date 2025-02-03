399e60de4c5e3f6bf4c27b1f1439e047c4a997a8
Jasper Frumau <jasper@imagewize.com>
2025-02-03 15:04:19 +0700

    icon change

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => '07727290634c8a208898');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => 'beb2941f21df92859738');
