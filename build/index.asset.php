320bbf8ef2a903151c3ffdcad039ba1b9586048b
Jasper Frumau <jasper@imagewize.com>
2025-02-04 13:42:13 +0700

    styling update with regular border support usage

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => '6ff8b81a0eeda6bd09a3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => 'caf39c8b090611fbbb65');
