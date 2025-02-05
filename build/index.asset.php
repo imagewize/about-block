11c572cca82b97d14cd5c5c658deb6b0ad13ee10
Jasper Frumau <jasper@imagewize.com>
2025-02-05 08:39:51 +0700

    alignfull for about block as default

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => 'eea2bd5b4f5ed04db295');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-i18n'), 'version' => 'e2896e81071a97bbe46d');
